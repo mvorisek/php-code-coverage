78f9f8138a2b9b450d1d022acea5467a6eb5d3f8
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-17 00:41:46 +0100

    revert 964 PR changes

--- src/CodeCoverage.php
+++ src/CodeCoverage.php
@@ -485,16 +485,9 @@ final class CodeCoverage
                 continue;
             }
 
-            $linesToBranchMap = $this->analyser()->executableLinesIn($filename);
-
             $data->keepLineCoverageDataOnlyForLines(
                 $filename,
-                array_keys($linesToBranchMap)
-            );
-
-            $data->markExecutableLineByBranch(
-                $filename,
-                $linesToBranchMap
+                $this->analyser()->executableLinesIn($filename)
             );
         }
     }
